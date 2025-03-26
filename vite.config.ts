29ff5e95b47550b5188b6b4db02afefb91cfaf00
huzaifa-vartana <huzaifa@vartana.com>
2025-03-25 22:13:51 -0400

    Remove base path from Vite configuration and add CNAME file for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/project-paradise/',
   server: {
     host: "::",
     port: 8080,
