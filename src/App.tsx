5f0579c2ac02668f90dc05044fb613f4247dd3ba
huzaifa-vartana <huzaifa@vartana.com>
2025-03-26 23:59:44 -0400

    Update SpeedInsights import path in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog";
